f9b5b4eeaea579ec02731285a9f7ad6edd4a4363
Tommy Doak <tcdoak@gmail.com>
2026-05-04 21:41:23 +0200

    add build to gitignore

--- build/lib/tools/schema.js
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod';
-export const QuerySchema = z
-    .string()
-    .trim()
-    .describe('Search query from the user to find relevant content.')
-    .default("Get Tommy Doak's work experience?");
