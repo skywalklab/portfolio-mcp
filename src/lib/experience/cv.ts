276db3e16bb04540c38014070679b9d75b860736
Tommy Doak <tommy.doak@skywalklab.com>
2026-06-25 10:18:49 +0200

    replace deprecated lucide icons

--- src/lib/experience/cv.ts
+++ src/lib/experience/cv.ts
@@ -36,20 +36,19 @@ export const contactPoints: ContactPoint[] = [
 	{
 		type: 'Website',
 		text: 'skywalklab.com/hire-tommy-doak',
-		// lucide_icon: "globe",
 		img: 'https://skywalklab.com/brand/logo-96.png',
 		href: 'https://skywalklab.com/hire-tommy-doak'
 	},
 	{
 		type: 'LinkedIn',
 		text: '/in/tommydoak',
-		lucide_icon: 'linkedin',
+		img: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3E%3Cpath d='M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z'/%3E%3Crect width='4' height='12' x='2' y='9'/%3E%3Ccircle cx='4' cy='4' r='2'/%3E%3C/svg%3E",
 		href: 'https://www.linkedin.com/in/tommydoak/'
 	},
 	{
 		type: 'Github',
 		text: '/skywalklab',
-		lucide_icon: 'github',
+		img: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3E%3Cpath d='M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4'/%3E%3Cpath d='M9 18c-4.51 2-5-2-7-2'/%3E%3C/svg%3E",
 		href: 'https://github.com/skywalklab/'
 	}
 ];
